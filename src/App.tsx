0023eea299ceedf4cb624e9b863f18db9c1f5741
Victor Rabelo <victorabelo1697@gmail.com>
2020-07-16 15:34:29 -0300

    :sparkles: global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const App: React.FC = () => {
-  return <h1>Hello GoBarber</h1>;
-};
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <GlobalStyle />
+    <h1>Hello GoBarber</h1>
+  </>
+);
 
 export default App;
