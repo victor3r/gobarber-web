28f31f95e95c569be2edc5f3beb1d61f3d41d221
Victor Rabelo <victorabelo1697@gmail.com>
2020-07-20 15:36:14 -0300

    :sparkles: Context API

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,15 @@ import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
+import AuthContext from './context/AuthContext';
+
 const App: React.FC = () => (
   <>
+    <AuthContext.Provider value={{ name: 'Victor' }}>
+      <SignIn />
+    </AuthContext.Provider>
+
     <GlobalStyle />
-    <SignIn />
   </>
 );
 
